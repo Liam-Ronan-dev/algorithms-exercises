e94323a9d009fa476a5476d43a831d21262764f4
Liam-Ronan-dev <liamronan16@gmail.com>
2025-06-23 17:26:16 +0100

    added bubble sort solutions

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -11,12 +11,41 @@
 */
 
 function bubbleSort(nums) {
-  // code goes here
+  let swapped = false;
+
+  do {
+    swapped = false;
+
+    for(let i=0; i < nums.length; i++) {
+      if(nums[i] > nums[i + 1]) {
+        const temp = nums[i]
+        nums[i] = nums[i + 1]
+        nums[i + 1] = temp;
+        swapped = true
+      }
+    }
+  } while(swapped);
+
+  return nums;
+
+
+  
+  // for(let i=0; i < nums.length; i++) {
+  //   for(let j=i+1; j < nums.length; j++) {
+  //     if(nums[i] > nums[j]) {
+  //       const temp = nums[i]
+  //       nums[i] = nums[j]
+  //       nums[j] = temp
+  //     }
+  //   }
+  // }
+  // return nums;
 }
+
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test.only("bubble sort", function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
